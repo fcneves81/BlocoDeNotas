d353874475e33085a50415c282363d0b82a5fd55
fcneves81 <fcneves81@gmail.com>
2023-11-12 17:40:16 -0300

    Criada a pasta views

---
+++ BlocoDeNotas/Views/PaginaNota.xaml.cs
@@ -0,0 +1,29 @@
+namespace BlocoDeNotas.Views;
+
+public partial class PaginaNota : ContentPage
+{
+    string _filename = Path.Combine(FileSystem.AppDataDirectory, "notas.txt");
+	public PaginaNota()
+	{
+		InitializeComponent();
+        if (File.Exists(_filename))
+        {
+            TxtEditor.Text = File.ReadAllText(_filename);
+        }
+	}
+
+    private void BtnSalvar_Clicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_filename, TxtEditor.Text);
+    }
+
+    private void BtnExcluir_Clicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_filename))
+        {
+            File.Delete(_filename);
+        }
+        TxtEditor.Text = string.Empty;
+
+    }
+}
